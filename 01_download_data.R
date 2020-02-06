5bf51fedff0b34db4895474745f684211569acf4
saramortara <saramortara@gmail.com>
2020-02-06 18:52:24 -0300

    editing gitignore

--- 01_download_data.R
+++ 01_download_data.R
@@ -1,2 +1,14 @@
 # Tutorial for data cleaning
 
+# loading packages
+
+library(rgbif)
+
+# basic search
+
+species <- "Cariniana legalis"
+
+occs <- occ_search(scientificName = species,
+                   return = "data")
+
+head(occs)
